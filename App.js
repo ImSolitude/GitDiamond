6966e22e0a156b7277170eb32a6d003e25f984d6
MuhammadJ <muhammadcodez@gmail.com>
2019-06-06 13:34:11 +0300

    v1.0

---
+++ App.js
@@ -0,0 +1,15 @@
+/* global console, alert, confirm  */
+const contact = document.querySelector('.contact');
+const backToTop = document.querySelector('.back-to-top');
+const nav = document.querySelector('nav');
+
+window.addEventListener('scroll', () => {
+  scrollY < 10
+    ? nav.classList.remove('scrolled')
+    : nav.classList.add('scrolled');
+
+  const coords = contact.getBoundingClientRect();
+  coords.top < 900
+    ? backToTop.classList.add('show')
+    : backToTop.classList.remove('show');
+});
